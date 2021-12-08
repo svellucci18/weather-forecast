cf01a23f7d503bfff57f82faba8b381e6d52de85
svell <svellucci18@gmail.com>
2021-12-08 12:06:42 -0800

    in class demo

---
+++ assets/javascript/script.js
@@ -0,0 +1,23 @@
+// From the <form> element, listen to the "submit"
+
+    // Select <input>, get its value, and provide it to the geo API
+
+// From the <button> container element, listen to the <button> "click"
+
+    // Get the city from the button's data attribute
+
+// Fetch the geo data (lat, lon) (city name)
+
+    // q = Name of the city as a query string (key value pairs)
+    // limit = 5 cities (optional parameter)
+    // appid = Your custom API key (make an account and then add it here)
+
+// Fetch the one call weather data
+
+    // lat
+    // lon
+    // appid
+    // units = imperial
+    // exclude = minutely,hourly
+    
+// Render the weather data to the page
