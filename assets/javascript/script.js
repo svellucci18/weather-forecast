4cdc6912a35a7770d8a08abc3feb492eac87bab1
svell <svellucci18@gmail.com>
2021-12-09 18:10:47 -0800

    tutor session, massive progress

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,4 +1,7 @@
 // From the <form> element, listen to the "submit"
+var searchBtn = $("#searchBtn");
+
+searchBtn.click(renderWeather);
 
     // Select <input>, get its value, and provide it to the geo API
 
@@ -19,7 +22,7 @@ function geoData(cityName){
 
             console.log(data);
 
-            oneCall( /* provide lat and lon here */ ); // Call oneCall here so that we are waiting for the data from the city name
+            oneCall( data[0].lat, data[0].lon ); // Call oneCall here so that we are waiting for the data from the city name
 
         })
 
@@ -50,10 +53,43 @@ function oneCall(lat,lon){
             console.log(data);
             
             // Render the weather data to the page
+            // Print the daily weather
+            var dailyWeatherEl = $("#dailyWeather");
+
+            dailyWeatherEl.html("");
+            dailyWeatherEl.append( 
+                `<li class="collection-header"><h4>${cityName.val()} ${moment().format("dddd, MMMM Do")}</h4></li>
+                <li class="collection-item">Temp: ${data.daily[0].temp.max}</li>
+                <li class="collection-item">Wind:</li>
+                <li class="collection-item">Humidity:</li>
+                <li class="collection-item">UV Index:</li>`);
 
         })
 
 }
     
+// Print/Render the weather data to the page and save searches
+var cityName = $("#cityInput");
 
-// Print/Render the weather data to the page
+function renderWeather(event){
+    console.log(event.target);
+    geoData(cityName.val());
+
+    var cityBtns = $("#city-buttons")
+
+    cityBtns.append(
+        `<button data-city=${cityName.val()} class=" prevCity btn blue darken-2">${cityName.val()}</button>
+        <br>`
+    )
+};
+
+function renderWeather2(event){
+    console.log(event.target);
+    geoData(event.target.innerText);
+};
+
+// Event Listener for the new city buttons
+var previousCityBtn = document.querySelectorAll(".prevCity");
+for (var i=0; i < previousCityBtn.length; i++) {
+    previousCityBtn[i].click(renderWeather2);
+}
