5f74547210d69c1f9d8ec24a0e975dabb68f6a0a
svell <svellucci18@gmail.com>
2021-12-15 18:22:18 -0800

    changed geoData link to https

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -10,7 +10,7 @@ var cityName = "";
 // Fetch the geo data (lat, lon) (city name) http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
 function geoData(city){
 
-    var url = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=949ac09a20eff646d7416ecc12078edc`;
+    var url = `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=949ac09a20eff646d7416ecc12078edc`;
     // q = Name of the city as a query string (key value pairs)
     // limit = 5 cities (optional parameter)
     // appid = Your custom API key (make an account and then add it here) 949ac09a20eff646d7416ecc12078edc
